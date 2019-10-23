df2740c306c08bf5a9226b6b040c0545a5fdcc71
guided <bill.coury@gmail.com>
2019-10-23 13:16:26 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # FriendFinder
+
+A fullstack friend-finding application using the Express package for Node.js
+
+Check out the deployed app on Heroku here!
+
+The app uses Express to serve 2 HTML files and 2 API routes to get and post data.
+
+Note that data is not persistent, but rather stored in an object in friends.js for your current session.
+
+# Instructions
+After cloning the repo, run npm install to download the Node dependencies. Launch the app in localhost:8080 in your browser.
---
+++ app/data/friends.js
@@ -0,0 +1,42 @@
+var friendsData = [
+    {
+      name: "Neko Case",
+      photo: "https://www.billboard.com/files/styles/768x433/public/media/01-neko-case-fea-4sr2c-2018-u-billboard-fea-1500.jpg",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Gandalf",
+      photo:"https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Gandalf600ppx.jpg/220px-Gandalf600ppx.jpg",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Robert Pollard",
+      photo:"http://magnetmagazine.com/wp-content/uploads/2008/12/pollard_vc.jpg",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Chewbacca",
+      photo:"https://lumiere-a.akamaihd.net/v1/images/solo-chewbacca-main_80768fa8.jpeg?region=8%2C0%2C1543%2C868&width=960",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Stan Marsh",
+      photo:"http://pm1.narvii.com/6150/e850378ac91d9487fd1ad20571388378ddc825d6_00.jpg",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Mia Wallace",
+      photo:"https://s-media-cache-ak0.pinimg.com/originals/a9/01/29/a9012982eea651ee31e21f4ad11fe96b.jpg",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "Princess Leia",
+      photo:"http://images.techtimes.com/data/images/full/168082/princess-leia-organa.jpg?w=600",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
